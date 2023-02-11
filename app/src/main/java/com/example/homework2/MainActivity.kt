14ab5fe4790a042665de0e74f735570df48bb237
Andrey Koroliov <korol1981an@gmail.com>
2023-02-12 00:10:54 +0300

    task 2 without star done

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.homework2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.homework2.databinding.ActivityMainBinding
 
+
+const val CURRENT_STATE = "current state"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
@@ -15,9 +16,19 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         binding.btnNextActivity.setOnClickListener{
-            Log.d("my_tag","press next button")
             val intent = Intent(this, SecondActivity::class.java)
+            val currentState = binding.tvMessage.text.toString()
+            intent.putExtra(CURRENT_STATE, currentState)
             startActivity(intent)
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val message = intent?.getStringExtra(EXTRA_MESSAGE)
+        message?.let {
+            binding.tvMessage.text = it
+        }
+
     }
 }
--- app/src/main/java/com/example/homework2/SecondActivity.kt
+++ app/src/main/java/com/example/homework2/SecondActivity.kt
@@ -1,11 +1,28 @@
 package com.example.homework2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homework2.databinding.ActivityMainBinding
+import com.example.homework2.databinding.ActivitySecondBinding
 
+const val EXTRA_MESSAGE = "extra message"
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnBack.setOnClickListener {
+            var message = binding.etInputData.text.toString()
+            if (message == "") {
+                message = intent?.getStringExtra(CURRENT_STATE).toString()
+            }
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(EXTRA_MESSAGE, message)
+            startActivity(intent)
+        }
     }
 }
