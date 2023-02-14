f1e5eb4f9638d3d61127a0a001912d8ec606bd40
Andrey Koroliov <korol1981an@gmail.com>
2023-02-14 21:44:46 +0300

    home work 3_1 done

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.homework2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homework2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+    }
+    private fun setListeners(){
+        binding.buttonToFrameLayout.setOnClickListener {
+            val intent = Intent(this,FrameActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonToLinearLayout.setOnClickListener {
+            val intent = Intent(this,LinearActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonToGridLayout.setOnClickListener {
+            val intent = Intent(this,GridActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
