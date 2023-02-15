5d405c6848a300a6820a242944ad63fc412b48b7
Andrey Koroliov <korol1981an@gmail.com>
2023-02-15 13:55:00 +0300

    homework 3_2 done without star

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import com.example.homework2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+        binding.btnContacts.setOnClickListener {
+            binding.groupOfficeAndButton.isVisible = false
+            binding.progressBar.isVisible = true
+        }
+
+        binding.btnGallery.setOnClickListener {
+            binding.groupOfficeAndButton.isVisible = false
+            binding.progressBar.isVisible = true
+        }
+
+        binding.btnVacancies.setOnClickListener {
+            binding.groupOfficeAndButton.isVisible = false
+            binding.progressBar.isVisible = true
+        }
+
+        binding.progressBar.setOnClickListener {
+            binding.groupOfficeAndButton.isVisible = true
+            binding.progressBar.isVisible = false
+        }
     }
 }
